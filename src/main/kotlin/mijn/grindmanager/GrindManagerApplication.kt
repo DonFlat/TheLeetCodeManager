aa83461334cbc3ab1e181f053e1ea0182f134448
dun <x.shi.2@umail.leidenuniv.nl>
2024-02-29 19:07:15 +0100

    Initial commit

---
+++ src/main/kotlin/mijn/grindmanager/GrindManagerApplication.kt
@@ -0,0 +1,11 @@
+package mijn.grindmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrindManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<GrindManagerApplication>(*args)
+}
---
+++ src/test/kotlin/mijn/grindmanager/GrindManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package mijn.grindmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrindManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
